725224570d050165835646dd9a46cd815a65a2ad
yuriy <knifeparty4@gmail.com>
2018-02-21 23:56:18 +0300

    new view logic added

--- src/main/webapp/WEB-INF/pages/app.js
+++ src/main/webapp/WEB-INF/pages/app.js
@@ -5,18 +5,36 @@
 
     function AppController($http) {
         var vm = this;
+        vm.fileName = '';
+        vm.canShowDataForOneFile = false;
+        vm.hasErrorMessage = false;
 
         vm.hasError = hasError;
         vm.getTime = getTime;
+        vm.getDataForOneFile = getDataForOneFile;
 
         init();
-
 
         function init() {
             $http.get('/result/all')
                 .then(function (resp) {
                     vm.data = resp.data;
-                })
+                });
+        }
+        
+        function getDataForOneFile() {
+            $http({
+                method: 'GET',
+                url: '/result',
+                params: {date: vm.fileName}
+            }).then(function (resp) {
+                vm.singleFile = resp.data;
+                vm.canShowDataForOneFile = true;
+                vm.hasErrorMessage = false;
+            }).catch(function (err) {
+                vm.hasErrorMessage = true;
+                vm.canShowDataForOneFile = false;
+            });
         }
         
         function getTime(time) {
